cdd8cacb6127baac2af506e7ab00fb82c967377a
Antoine Lehurt <keuwah@gmail.com>
2013-03-23 21:22:20 +0100

    Improve performance
    http://jsperf.com/function-versus-function-call-versus-function-apply

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 /**
  * Iterates an array.
+ * If the iterator return false, it breaks the loop.
  *
  * @param  {Array} arr
  * @param  {Function} fn    Iterator
- * @param  {Object} ctx     Context
+ * @param  {Object} ctx     [Optional] Context
  * @return {Array}
  */
 module.exports = function forEach(arr, fn, ctx) {
-  for (var i = 0, len = arr.length; i < len; i++) {
-    // if the iterator return false,
-    // it breaks the loop
-    if (false === fn.call(ctx, arr[i], i, arr)) {
-      return arr;
+  var len = arr.length;
+  var i;
+
+  if (!ctx) {
+    for (i = 0; i < len; i++) {
+      if (false === fn(arr[i], i, arr)) {
+        return arr;
+      }
+    }
+  } else {
+    for (i = 0; i < len; i++) {
+      if (false === fn.call(ctx, arr[i], i, arr)) {
+        return arr;
+      }
     }
   }
 
